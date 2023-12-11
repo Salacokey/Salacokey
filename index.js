948a57759b3b85b88ed496f187a02b8809b11c90
Salacokey <2073399165@qq.com>
2023-12-11 14:29:50 +0800

    功能7

--- index.js
+++ index.js
@@ -3,8 +3,7 @@ console.log('功能2');
 console.log('功能3');
 console.log('功能4');
 console.log('功能4');
-<<<<<<< HEAD
 console.log('完成功能5');
-=======
 console.log('完成功能6');
->>>>>>> feat-6
+
+console.log(功能7);
